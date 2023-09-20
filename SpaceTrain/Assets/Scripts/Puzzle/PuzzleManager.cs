33f29ab6e0747eb15c28ca8fdb1c44263ffd938b
DM <dommcneill@outlook.com>
2023-09-20 15:13:25 +0100

    scene management, puzzles in wip

---
+++ SpaceTrain/Assets/Scripts/Puzzle/PuzzleManager.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PuzzleManager : MonoBehaviour
+{
+	public static PuzzleManager Instance { get; private set; }
+
+	public Dictionary<int, bool> Objectives = new Dictionary<int, bool>();
+
+	public GameObject Door;
+
+	void Awake()
+	{
+		if (Instance != null && Instance != this)
+		{
+			//Destroy(this)
+		}
+		else
+		{
+			Instance = this;
+		}
+	}
+
+	void Update()
+	{
+		if (Objectives.Count <= 0 || Door == null) return;
+
+		bool allCompleate = false;
+
+		for (int i = 0; i < Objectives.Count; i++)
+		{
+			if (Objectives[i] == true)
+			{
+				allCompleate = true;
+			}
+			else
+			{
+				allCompleate = false;
+				//continue;
+				break;
+			}
+
+		}
+
+		print(allCompleate);
+
+		if (allCompleate == true)
+		{
+			Door.SetActive(false);
+		}
+		else
+		{
+			if (!Door.activeSelf) Door.SetActive(true);
+		}
+	}
+
+	public void AddToCollection(int ID, bool value = false)
+	{
+		Objectives.Add(ID, value);
+	}
+
+	public void RemoveFromCollection(int ID)
+	{
+		Objectives.Remove(ID);
+	}
+
+	public void UpdateValueInCollection(int ID, bool value)
+	{
+		Objectives[ID] = value;
+	}
+
+
+
+}
